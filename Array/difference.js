25d9c5d6684f9bd5644245115f6be7050db08ccb
ctrlf <lfy3715@gmail.com>
2017-08-17 17:44:53 +0800

    加入dfff

---
+++ Array/difference.js
@@ -0,0 +1,18 @@
+/**
+ * Created by king on 2017/8/17.
+ */
+
+/**
+ * 创建一个具有唯一array值的数组，每个值不包含在其他给定的数组中。即创建一个新数组，这个数组中的值，为第一个数字（array 参数）排除了给定数组中的值。
+ * 参数
+ *    array (Array): 要检查的数组。
+ *   [values] (...*): 排除的值。
+ * 返回值
+ *   (Array): 返回一个过滤值后的新数组。
+ */
+
+const difference = baseRest(function (array, values) {
+    // return
+});
+    
+function baseDifference()
